17605dc8f66c2f5feaa1a6080c12430f8617881f
juansimonEAFIT <jsospinam@eafit.edu.co>
2025-08-01 11:37:04 -0500

    Busqueda de peliculas

--- movie/views.py
+++ movie/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Movie
+
 # Create your views here.
 def home(request):
     #return HttpResponse("<h1>Welcome to home page</h1>")
     #return render(request, 'home.html')
-    return render(request, 'home.html', {'name': 'Juan Simón Ospina'})
+    #return render(request, 'home.html', {'name': 'Juan Simón Ospina'})
+    searchterm = request.GET.get('searchMovie')
+    if searchterm:
+        movies = Movie.objects.filter(title__icontains=searchterm)
+    else:
+        movies = Movie.objects.all()
+    return render(request, 'home.html', {'searchterm': searchterm, 'movies': movies})
+
 
 def about(request):
     return render(request, 'about.html', {'name': 'Juan Simón Ospina'})
